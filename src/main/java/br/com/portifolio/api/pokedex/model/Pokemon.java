dd5340465c0fd1efd988b2111be37aff78e1f52c
Gentil Silva <silvagentil.sg@gmail.com>
2023-05-10 02:55:23 -0300

    Configurando o arquivo properties, Crianto entidade Pokemon e ORM

---
+++ src/main/java/br/com/portifolio/api/pokedex/model/Pokemon.java
@@ -0,0 +1,21 @@
+package br.com.portifolio.api.pokedex.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Pokemon")
+@Table(name = "tb_pokemon")
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Pokemon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Float altura;
+    private Float peso;
+    private String urlImagem;
+
+}
